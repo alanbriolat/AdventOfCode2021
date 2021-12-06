0dff9fc9c7deeb9de487fd600769b375a7600331
Alan Briolat <alan@briol.at>
2021-12-06 19:21:52 +0000

    day06: part 2, and less naive (memoised recursive)

--- src/solutions/day06.rs
+++ src/solutions/day06.rs
@@ -1,33 +1,52 @@
-use super::prelude::*;
-use crate::util::{parse_lines, read_file};
+use std::collections::HashMap;
 
-const SPAWN_INTERVAL: usize = 7;
+use super::prelude::*;
+use crate::util::read_file;
+
+const SPAWN_INTERVAL: u64 = 7;
 const SPAWN_DELAY: u64 = 9;
 
-fn recursive_count(initial_timer: u64, end: u64) -> u64 {
-    1u64 + (initial_timer..end)
-        // Get each time this lanternfish with spawn
-        .step_by(SPAWN_INTERVAL)
-        // Get the descendents of each of those, recursively
-        .map(|spawn_at| recursive_count(spawn_at + SPAWN_DELAY, end))
-        .sum::<u64>()
+fn read_input(line: &str) -> impl Iterator<Item = u64> + '_ {
+    line.split(',').map(|x| x.parse::<u64>().unwrap())
+}
+
+fn run<I: Iterator<Item = u64>>(input: I, days: u64) -> u64 {
+    let mut memo: HashMap<u64, u64> = HashMap::new();
+
+    fn count(initial: u64, end: u64, memo: &mut HashMap<u64, u64>) -> u64 {
+        if let Some(result) = memo.get(&initial).copied() {
+            result
+        } else {
+            let result = (initial..end)
+                // Get each time this lanternfish will spawn before the end
+                .step_by(SPAWN_INTERVAL as usize)
+                // Get the descendents of each of those, recursively
+                .map(|spawn_at| count(spawn_at + SPAWN_DELAY, end, memo))
+                .sum::<u64>()
+                // Include self
+                + 1;
+            memo.insert(initial, result);
+            result
+        }
+    }
+
+    input.map(|t| count(t, days, &mut memo)).sum()
 }
 
 fn part1<R: BufRead>(reader: R) -> crate::Result<String> {
-    let input: Vec<u64> = reader
-        .lines()
-        .next()
-        .unwrap()
-        .unwrap()
-        .split(',')
-        .map(|x| x.parse::<u64>().unwrap())
-        .collect();
-    let total: u64 = input.iter().copied().map(|t| recursive_count(t, 80)).sum();
+    let total = run(
+        read_input(reader.lines().next().unwrap().unwrap().as_str()),
+        80,
+    );
     Ok(total.to_string())
 }
 
 fn part2<R: BufRead>(reader: R) -> crate::Result<String> {
-    todo!()
+    let total = run(
+        read_input(reader.lines().next().unwrap().unwrap().as_str()),
+        256,
+    );
+    Ok(total.to_string())
 }
 
 pub fn build_runner() -> crate::Runner {
@@ -63,8 +82,11 @@ mod tests {
                 3,4,3,1,2
             "}))
             .unwrap(),
-            "???"
+            "26984457539"
         );
-        assert_eq!(part2(read_file("data/day06_input.txt")).unwrap(), "???");
+        assert_eq!(
+            part2(read_file("data/day06_input.txt")).unwrap(),
+            "1765974267455"
+        );
     }
 }
